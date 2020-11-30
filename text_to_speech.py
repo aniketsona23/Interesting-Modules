79dadbd7b5a4a1b53461724cc20d46a39ccd7046
aniketsona23 <72734761+aniketsona23@users.noreply.github.com>
2020-12-01 00:47:31 +0530

    Add files via upload

---
+++ text_to_speech.py
@@ -0,0 +1,11 @@
+import pyttsx3 as pt
+
+#Not required if dont want to change voice properties
+#engine = pt.init()
+#voice=engine.getProperty('voices')
+#engine.setProperty("voices",voice[0])
+#engine.setProperty("rate",30)
+# engine.setProperty('voices', voice[1])
+
+pt.speak("Turned on! I'm Jarvis. Ready to assist you for your journey!")
+#
