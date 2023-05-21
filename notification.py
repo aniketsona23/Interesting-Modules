775b047ea5889945e0951aa2f070476ebe0b1554
Aniket Sonawane <72734761+aniketsona23@users.noreply.github.com>
2023-05-22 00:49:53 +0530

    Update notification.py
    Adding steps to install required library

--- notification.py
+++ notification.py
@@ -1,3 +1,7 @@
+
+#Enter following command in cmd/terminal to install win10toast library
+#pip install win10toast
+
 from win10toast import ToastNotifier
 
 # .show_toast(title,msg,icon_path,duration)
